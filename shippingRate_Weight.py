ac83afba43cc085b6670bd309cdb56186715d87f
Haji001 <95364914+Haji001@users.noreply.github.com>
2023-05-22 13:39:53 -0400

    Update ShippingRateWeight.py

--- shippingRate_Weight.py
+++ shippingRate_Weight.py
@@ -1,39 +1,61 @@
-
-# Package weight
-weight = 80
+import random
 
 
-# Ground Shipping
+# ------------------------Ground shipping-----------------------------# 
 
-if weight <= 2:
-  cost_of_ground = weight * 1.50 + 20
+destinationTax = .005
+maxWeight = 120
 
-elif weight <= 6:
-  cost_of_ground = weight * 3.00 + 20
 
-elif weight <= 10:
-  cost_of_ground = weight * 4.00 + 20
+States = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
+  "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
+  "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
+  "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
+  "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
+  "North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
+  "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
+  "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
 
-elif weight > 10:
-  cost_of_ground = weight * 4.75 + 20
 
-print("Ground Shipping is ${0}".format(cost_of_ground))
 
-### Drone Shipping!
+loc = str(input("Where you shipping to? \n"))
 
-if weight <= 2:
-  cost_of_drone = weight * 4.50
+packageWeight = int(input("How much your package weight? \n"))
+  
+if loc in States: 
+  if packageWeight in range(1, 10):
+     costofShipping = packageWeight * destinationTax + random.randint(5, 10)
+                                                      # \\ Price ranges between $5 to $10
 
-elif weight <= 6:
-  cost_of_drone = weight * 9.00
+  if packageWeight in range(10, 20):
+    costofShipping = packageWeight * destinationTax + random.randint(10, 15)
+    
+  if packageWeight in range(20,30):
+    costofShipping = packageWeight * destinationTax + random.randint(20, 25)
 
-elif weight <= 10:
-  cost_of_drone = weight * 12.00
+  if packageWeight in range(30, 40):
+    costofShipping = packageWeight * destinationTax + random.randint(30, 35)
 
-elif weight > 10:
-  cost_of_drone = weight * 14.25
+  if packageWeight in range(40, 50):
+    costofShipping = packageWeight * destinationTax + random.randint(35, 40)
 
-print("Drone Shipping is ${0}".format(cost_of_drone))
+  if packageWeight in range(50, 60):
+    costofShipping = packageWeight * destinationTax + random.randint(40, 45)
 
-### Ground Shipping Premium
+  if packageWeight in range(60, 70):
+    costofShipping = packageWeight * destinationTax + random.randint(50, 55)
+
+  if packageWeight in range(70, 80):
+    costofShipping = packageWeight * destinationTax + random.randint(60, 65)
+
+  if packageWeight in range(80, 90):
+    costofShipping = packageWeight * destinationTax + random.randint(70, 75)
+
+  if packageWeight in range(90, 100):
+    costofShipping = packageWeight * destinationTax + random.randint(80, 85)
+    
+print("Your shipping total is ${0}".format(costofShipping))
+    
+
+  
 
